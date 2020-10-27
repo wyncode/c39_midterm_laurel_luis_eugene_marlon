023227b602ee461d7b76eda5a1da265381414797
Marlon Angulo <Marlonangulo001@gmail.com>
2020-10-27 15:53:34 -0400

    added n new css

--- client/src/components/AlbumCard.jsx
+++ client/src/components/AlbumCard.jsx
@@ -1,31 +1,32 @@
 import React from "react"; 
 import Card from 'react-bootstrap/Card';
 import {useHistory} from 'react-router-dom';
-import Cards from './AlbumCardPage';
+import AlbumCardPage from './AlbumCardPage';
 
 
 
 const AlbumCard = ({id, albumname, artistname, image}) => { 
   
+  //whats this doing??
     const history = useHistory()
 
     const handleClick = (id) => { 
      history.push(`/albumpage/${id}`)
     }
 return ( 
-      <div>
-        <div className='cards__item'>
+      <>
+        <li list-style-type-none className='cards__item'>
           <Card className='cards__item__link' style={{ width: 150, margin: 10}} onClick={() => handleClick(id)}>
             <figure className='cards__item__pic-wrap'>
-              <Card.Img src={image} alt="Album Image"/>
+              <Card.Img className='cards__item__img' src={image} alt="Album Image"/>
             </figure>
             <Card.Body className='cards__item__info'>
-              <Card.Title><h5 className='cards__item__text'>{albumname}</h5></Card.Title>
-              <Card.Text><h5 className='cards__item__text'>{artistname}</h5></Card.Text>  
+              <Card.Title><h5 className='cards__album__text'>{albumname}</h5></Card.Title>
+              <Card.Text><p className='cards__artist_text'>{artistname}</p></Card.Text>  
             </Card.Body>
           </Card>
-        </div>
-      </div>
+        </li>
+      </>
       );
      
 }
