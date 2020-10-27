6168f08a063824326d997f2703c759a2cf151c1f
ljgalaty <laurel.galaty@gmail.com>
2020-10-27 11:53:34 -0400

    added byrdsong logo

--- client/src/components/NavMenu.jsx
+++ client/src/components/NavMenu.jsx
@@ -4,6 +4,7 @@ const NavMenu = () => {
   
   return (
     <div class="sidenav">
+      <img src="./logo.png" alt="byrdsong logo"/>
       <ul>
         <li href="/">Albums</li>
         <li href="/aboutus">About Us</li>
