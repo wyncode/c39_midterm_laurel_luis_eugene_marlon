274425151e0df789f720254f5fe67d35208a0817
ljgalaty <laurel.galaty@gmail.com>
2020-10-28 18:03:33 -0400

    fixed about-us and background iamge

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -8,52 +8,45 @@ const AboutUs = () => {
   return (
    <div className="about-us">
       <h1>About Us</h1> 
-         <h2>Our Team</h2>
+         
+         
          <div className="column">
-            <div className="card">
-               <img/>
+            <div className="card ">
+               <img src="./src/components/images/LuisPerez.jpeg"/>
              <div className="container">
                <h2>Luis</h2>
-                  <p className="title">CEO & Founder</p>
-                     <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                     <p>jane@example.com</p>
-                        <p><button className="button">Contact</button></p>
+                  <p className="title">Developer</p>
             </div>
          </div>
       </div>
+      
+      
       <div className="column">
-            <div className="card">
-               <img/>
+            <div className="card ">
+               <img src="./src/components/images/IMG_9005.jpeg"/>
              <div className="container">
                <h2>Marlon</h2>
-                  <p className="title">CEO & Founder</p>
-                     <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                     <p>jane@example.com</p>
-                        <p><button className="button">Contact</button></p>
+                  <p className="title">Developer</p>
             </div>
          </div>
       </div>
+      
       <div className="column">
             <div className="card">
                <img/>
              <div className="container">
                <h2>Eugene</h2>
-                  <p className="title">CEO & Founder</p>
-                     <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                     <p>jane@example.com</p>
-                        <p><button className="button">Contact</button></p>
+                  <p className="title">Developer</p>
             </div>
          </div>
       </div>
+      
       <div className="column">
             <div className="card">
-               <img/>
+               <img src="./src/components/images/LaurelGalaty.jpeg"/>
              <div className="container">
-               <h2>Eugene</h2>
-                  <p className="title">CEO & Founder</p>
-                     <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                     <p>jane@example.com</p>
-                        <p><button className="button">Contact</button></p>
+               <h2>Laurel</h2>
+                  <p className="title">Developer</p>
             </div>
          </div>
       </div>
