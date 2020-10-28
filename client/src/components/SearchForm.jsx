0a9bff86cd6d7d0c0b3b77b610c4c5571c97890a
EugeneW9 <washingtonway89@gmail.com>
2020-10-28 17:01:12 -0400

    searchbaredit

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -2,25 +2,28 @@ import React from "react"
 import Form from 'react-bootstrap/form';
 import "./searchForm.css";
 
+
 // Finish Design
 
 const SearchForm = ({ handleSubmitProp }) => {
     return (
-
       
         <Form className="Welcome-page-form" onSubmit={handleSubmitProp} style={{ margin: '2rem' }} >
           <div className="welcome-page-searchbar">
-          <i className="fas fa-play"></i>
+              <a className="btn" href="#">
+                {/* <img id="btn" className="img" src="./logo.png" alt="byrdsong logo"/> */}
+                {/* <i id="btn" className="fas fa-play"></i> */}
+              </a> 
+            
+              
             
             <Form.Row>
              <Form.Control
                 className="welcome-input"
                 clasName="welcome-icon"
                 id="searchbar"
-                size="lg"
-                type="text"
+                type="text" 
                 placeholder="Find your Sound!"
-              
               ></Form.Control>
             </Form.Row>
           </div>
